4338061ced6b460f5eeee94b5e803d793f2caeea
Anastasiia <ilina@involve.software>
2023-12-23 23:48:14 +0200

    uncommented code

--- hw18/index.py
+++ hw18/index.py
@@ -3,17 +3,17 @@
 # довжини та ширини прямокутника. Використовуйте декоратор @property
 # для повернення площі прямокутника.
 
-# class Rectangle:
-#     def __init__(self, length, width):
-#         self.length = length
-#         self.width = width
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
 
-#     @property 
-#     def get_area(self):
-#         return self.length * self.width
+    @property 
+    def get_area(self):
+        return self.length * self.width
 
-# rectangle1 = Rectangle(10, 20)
-# print(rectangle1.get_area)
+rectangle1 = Rectangle(10, 20)
+print(rectangle1.get_area)
 
 # 2. Створіть дескриптор DiscountLimit, який визначає максимальний обсяг 
 # знижки для товару. Забороніть встановлення знижки більше, ніж встановлено за лімітом.
